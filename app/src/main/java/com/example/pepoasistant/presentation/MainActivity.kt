ee909a78be8410c950fa8fc335f0db7d940401a9
miakele <myakushkinaea@gmail.com>
2026-01-26 12:00:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pepoasistant/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pepoasistant.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pepoasistant.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.layout_main)
+    }
+}
