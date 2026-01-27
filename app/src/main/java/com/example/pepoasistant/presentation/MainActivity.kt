8ae9ca98fa13c536ea90c44e7ca971e31190c42e
miakele <myakushkinaea@gmail.com>
2026-01-27 07:47:25 +0200

    fragmentViews and item_view

--- app/src/main/java/com/example/pepoasistant/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pepoasistant/presentation/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.pepoasistant.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.pepoasistant.R
 
 class MainActivity : AppCompatActivity() {
