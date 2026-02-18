8d951cf47f40ca1680005d7995ece5587d423a85
miakele <myakushkinaea@gmail.com>
2026-02-18 17:11:42 +0200

    CategoryStatisticsAdapter.kt after review

--- app/src/main/java/com/example/pepoasistant/presentation/CategoryStatisticsAdapter.kt
+++ app/src/main/java/com/example/pepoasistant/presentation/CategoryStatisticsAdapter.kt
@@ -5,38 +5,26 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.example.pepoasistant.databinding.ItemCategotyStatisticsBinding
 
-class CategoryStatisticsAdapter() : ListAdapter<CategoryStatisticsUi, CategoryStatisticsViewHolder>(
-    CategoryStatisticsDiffCallBack()
-) {
-    private var maxAmount: Double = 1.0
+class CategoryStatisticsAdapter :
+    ListAdapter<CategoryStatisticsUi, CategoryStatisticsViewHolder>(CategoryStatisticsDiffCallBack()) {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): CategoryStatisticsViewHolder {
+    private var maxAmount: Double = 0.0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryStatisticsViewHolder {
         val binding = ItemCategotyStatisticsBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-
         return CategoryStatisticsViewHolder(binding)
     }
 
-    override fun onBindViewHolder(
-        holder: CategoryStatisticsViewHolder,
-        position: Int
-    ) {
-
-        holder.bind(getItem(position), maxAmount)
-
-
+    override fun onBindViewHolder(holder: CategoryStatisticsViewHolder, position: Int) {
+        holder.bind(currentList[position], maxAmount)
     }
 
-    override fun submitList(list: List<CategoryStatisticsUi?>?) {
-        if (!list.isNullOrEmpty()) {
-            maxAmount = list.maxOf { it?.amount ?: 0.0 }
-        }
+    override fun submitList(list: List<CategoryStatisticsUi>?) {
+        maxAmount = list?.maxOfOrNull { it.amount } ?: 0.0
         super.submitList(list)
     }
 }
